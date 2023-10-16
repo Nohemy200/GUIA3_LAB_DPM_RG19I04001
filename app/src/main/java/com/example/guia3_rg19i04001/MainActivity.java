16342db1dd420a2c383683a096389cdba771185e
Noemi <ramosgilma122@gmail.com>
2023-10-15 18:05:06 -0600

    configuracion de viewbinding e integracion de SharedPreferences

--- app/src/main/java/com/example/guia3_rg19i04001/MainActivity.java
+++ app/src/main/java/com/example/guia3_rg19i04001/MainActivity.java
@@ -2,22 +2,27 @@ package com.example.guia3_rg19i04001;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.guia3_rg19i04001.databinding.ActivityMainBinding;
 import com.example.guia3_rg19i04001.modelo.Trabajador;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     Button btnAgregar, btnMostrar, btnAcercaDe;
     public static ArrayList<Trabajador> lstTrabajadores;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         lstTrabajadores = new ArrayList<Trabajador>();
 
@@ -25,15 +30,15 @@ public class MainActivity extends AppCompatActivity {
         btnMostrar = findViewById(R.id.btnMostrar);
         btnAcercaDe = findViewById(R.id.btnAcercaDe);
 
-        btnAgregar.setOnClickListener(view -> {
+        btnAgregar.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, ElegirTipoTrabajadorActivity.class));
         });
 
-        btnMostrar.setOnClickListener(view -> {
+        btnMostrar.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, MostrarListaActivity.class));
         });
 
-        btnAcercaDe.setOnClickListener(view -> {
+        btnAcercaDe.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, AcercaDelProgramador.class));
         });
     }
